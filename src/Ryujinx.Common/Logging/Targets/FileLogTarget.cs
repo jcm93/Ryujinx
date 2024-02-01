76acb6ed52d0368bcefe699a863cc09021846d33
jcm <butt@butts.com>
2024-02-01 13:08:17 -0600

    Keep last five logs instead of three

--- src/Ryujinx.Common/Logging/Targets/FileLogTarget.cs
+++ src/Ryujinx.Common/Logging/Targets/FileLogTarget.cs
@@ -37,7 +37,7 @@ namespace Ryujinx.Common.Logging.Targets
 
             // Clean up old logs, should only keep 3
             FileInfo[] files = logDir.GetFiles("*.log").OrderBy((info => info.CreationTime)).ToArray();
-            for (int i = 0; i < files.Length - 2; i++)
+            for (int i = 0; i < files.Length - 4; i++)
             {
                 try
                 {
